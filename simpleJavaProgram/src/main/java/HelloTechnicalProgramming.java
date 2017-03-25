df2014a3600709c8850e47de1936437e86a015fc
Asanda Ketelo <aketelo@gmail.com>
2017-03-25 10:10:36 +0200

    AssignementOne Initail commit

---
+++ simpleJavaProgram/src/main/java/HelloTechnicalProgramming.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Phelokazi on 2017-03-25.
+ */
+public class HelloTechnicalProgramming {
+
+    public static void main(String args[]){
+
+        System.out.println("Hello Technical Programming");
+    }
+}
